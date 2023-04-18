88f8d313c3b62787ae8922271f444d36c5e02fb2
Ruslan Sakhautdinov <sakhrus1406199@gmail.com>
2023-04-18 15:40:35 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import './index.scss';
-import { Collection } from './collection';
+import { Collection } from './Collection';
 
 function App() {
- const [collections,setCollections] = React.useState ([])  
+ const [collections,setCollections] = React.useState ([]);
  
- React.useEffect(()=>{
-  fetch ('https://63eb18b7bfdd42996741406c.mockapi.io/collections').then(res =>res.json()).then(json=>{})
-    .then((res)=>res.json())
-    .then((json)=>{
-      setCollections(json)
+ React.useEffect(() => {
+  fetch('https://63eb18b7bfdd42996741406c.mockapi.io/collections').then(res => res.json()).then( json => {})
+    .then((res) => res.json())
+    .then((json) => { 
+      setCollections(json);
     })
-    .catch((err)=>{
-      console.warn('err');
-      alert('Ошибка при получении данных')
-    })
-       
+    .catch((err) => {
+      console.warn(err);
+      alert('отстой');
+    });
+    }, []);
 
-
-},[])
  return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
@@ -33,11 +31,13 @@ function App() {
         <input className="search-input" placeholder="Поиск по названию" />
       </div>
       <div className="content">
-        {collections.map((obj)=>(
-          <Collection  name={obj.name} images={obj.photos} />
-        ))}
-        
-
+        {collections.map((obj,index) => (
+          <Collection
+          key={index}
+          name={obj.name}
+          images={obj.photos}
+          />
+          ))}
       </div>
       <ul className="pagination">
         <li>1</li>
