0c5462bc27fe40462e4384310c26a097ccbc414f
Ruslan Sakhautdinov <sakhrus1406199@gmail.com>
2023-04-21 10:37:39 +0700

    handle was changed

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
 
  return (
     <div className="App">
-      <h1>Моя коллекция фотографий</h1>
+      <h1>Фотоколлекция</h1>
       <div className="top">
         <ul className="tags">
           {
